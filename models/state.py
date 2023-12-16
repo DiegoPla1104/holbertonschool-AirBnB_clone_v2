3accd78cee408569eb4dd0b52f6e9de1bf368fda
DiegoPla1104 <diegopla1104@gmail.com>
2023-12-16 02:38:28 +0000

    fix?

--- models/state.py
+++ models/state.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
 from models.city import City
 import models
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,24 @@
-#!/usr/bin/python3
-"""
-module script that starts Flask
-"""
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def display_states():
-    """
-    display a HTML page
-    list of all states and related cities
-    """
-    storage_states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=storage_states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method teardown"""
+def teardown(exception):
+    """Method to remove the current SQLAlchemy Session"""
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Display a HTML page with a list of states"""
+    storage_states = storage.all(State)
+    sorted_states = sorted(storage_states.values(), key=lambda x: x.name)
+
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
